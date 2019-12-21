870aef3a9c99152f6f7b68fabfedef6e9f6046b6
g-w1 <58830309+g-w1@users.noreply.github.com>
2019-12-21 11:34:27 -0500

    button change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
   var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
   var secondDate = new Date(2019, 6, 22);
   var diffDays = Math.round(Math.abs((date - secondDate) / oneDay));
-  var numberDone = Number(document.getElementById("input").value);
+  var numberDone = Number(document.getElementById("input1").value);
   var numberBurpees = ((diffDays * 25) +50 -numberDone);
   if (numberBurpees < 0){
   	numberBurpees = 0;
