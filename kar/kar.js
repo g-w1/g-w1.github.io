20c7a9536ce19175e6ff20d12b6a913e82bf37e3
g-w1 <jacoblevgw@gmail.com>
2020-12-01 15:22:11 -0500

    update

--- kar/kar.js
+++ kar/kar.js
@@ -1,13 +1,14 @@
- pressed()
-  function pressed(){let date = new Date();
-  var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+function pressed() {
+  let date = new Date();
+  var oneDay = 24 * 60 * 60 * 1000;
   var secondDate = new Date(2019, 6, 22);
   var diffDays = Math.round(Math.abs((date - secondDate) / oneDay));
   var numberDone = Number(document.getElementById("input1").value);
-  var numberBurpees = ((diffDays * 25) +50 -numberDone);
-  if (numberBurpees < 0){
-  	numberBurpees = 0;
+  var numberBurpees = ((diffDays * 25) + 50 - numberDone);
+  if (numberBurpees < 0) {
+    numberBurpees = 0;
   }
   document.getElementById('output').innerHTML = numberBurpees;
-    //just to show that jacob wrote this
-  document.getElementById('output1').innerHTML = date;}
+  document.getElementById('output1').innerHTML = date;
+}
+pressed()
