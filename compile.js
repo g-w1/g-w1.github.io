de0b1c69daeae319a40c36b81b948c614657a7b1
Jacob G-W <jacoblevgw@gmail.com>
2023-10-20 21:33:51 -0400

    add exercises.html

--- compile.js
+++ compile.js
@@ -13,23 +13,30 @@ const traverseDir = (dir, callback) => {
   });
 };
 
-const books = {
+let books = {
   jaynes: {
     title: "Probability Theory: The Logic of Science",
     author: "E. T. Jaynes",
     link: "https://www.amazon.com/Probability-Theory-Science-T-Jaynes/dp/0521592712",
+    exercises: [],
   },
 };
 
-const parseExercise = (filePath) => {
-  let content = fs.readFileSync(filePath, "utf8");
+function getBookAndExercise(filePath) {
   let base = path.basename(filePath);
   const parts = path.normalize(filePath).split(path.sep);
   const book = parts[parts.length - 2];
   const specific = books[book];
+  return [book, base];
+}
+
+const parseExercise = (filePath) => {
+  let content = fs.readFileSync(filePath, "utf8");
+  const [name, exercise] = getBookAndExercise(filePath);
+  const book = books[name];
   content = content.replace(/---/g, "<hr/>");
   content = content.replace(/\n\n/g, "<p>");
-  const meta = `${specific.title}; Exercise ${base}`;
+  const meta = `${book.title}; Exercise ${exercise}`;
   content =
     `
     <head>
@@ -38,7 +45,7 @@ const parseExercise = (filePath) => {
       <title>${meta}</title>
     </head>` +
     content +
-    `<hr> <small>Exercise ${base}, <a href="${specific.link}">${specific.title}</a>, ${specific.author}`;
+    `<hr> <small>Exercise ${exercise}, <a href="${book.link}">${book.title}</a>, ${book.author}`;
   return content;
 };
 
@@ -83,8 +90,23 @@ const writeHTML = (exercise, outputPath) => {
 };
 
 traverseDir("rawsolutions", (filePath) => {
-  const exercise = parseExercise(filePath);
   const outputPath = filePath.replace("rawsolutions", "solutions");
-
-  writeHTML(exercise, outputPath + ".html");
+  writeHTML(parseExercise(filePath), outputPath + ".html");
+  const [book, exercise] = getBookAndExercise(filePath);
+  books[book].exercises.push({ path: "/" + outputPath, num: exercise });
 });
+
+let exerciseshtml = `<head>
+      <meta name="description" content="Solutions to Math Exercises"/>
+      <link rel="stylesheet" type="text/css" href="/style.css">
+      <title>Solutions to Math Exercises</title>
+    </head>
+<h1>Solutions to Math Exercises</h1><hr>`;
+for (let bk in books) {
+  book = books[bk];
+  exerciseshtml += `<h2><a href="${book.link}">${book.title}</a>, ${book.author}</h2>`;
+  for (exercise of book.exercises.reverse()) {
+    exerciseshtml += `<p><a href="${exercise.path}">${exercise.num}</a></p>`;
+  }
+}
+fs.writeFileSync("./exercises.html", exerciseshtml);
